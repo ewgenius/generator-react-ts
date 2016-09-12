e5a8aa4275cbf7aa4394114e70fe4dbf55d2612f
ewgenius <ewgeniux@gmail.com>
2016-09-12 19:07:45 +0300

    init template

--- generators/app/index.js
+++ generators/app/index.js
@@ -9,12 +9,34 @@ module.exports = yeoman.Base.extend({
       'Welcome to the ' + chalk.red('react-ts') + ' generator!'
     ));
 
-    var prompts = [{
-      type: 'prompt',
-      name: 'appName',
-      message: 'enter app name',
-      default: 'app'
-    }];
+    var prompts = [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Enter app name',
+        default: 'app'
+      }, {
+        type: 'input',
+        name: 'description',
+        message: 'Enter app description',
+        default: ''
+      }, {
+        type: 'confirm',
+        name: 'useRedux',
+        message: 'Use redux',
+        default: false
+      }, {
+        type: 'confirm',
+        name: 'useRouter',
+        message: 'Use react-router',
+        default: false
+      }, {
+        type: 'confirm',
+        name: 'useMUI',
+        message: 'Use material-ui',
+        default: false
+      }
+    ];
 
     return this.prompt(prompts).then(function (props) {
       this.props = props;
@@ -22,13 +44,46 @@ module.exports = yeoman.Base.extend({
   },
 
   writing: function () {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
+    this.fs.copyTpl(
+      `${this.templatePath()}/**/!(_)*`,
+      this.destinationPath(),
+      this.props
+    );
+
+    this.fs.copyTpl(
+      `${this.templatePath()}/**/_package.json*`,
+      this.destinationPath('package.json'),
+      this.props
+    );
+  },
+
+  createingPackage: function () {
+    this.fs.writeJSON(
+      this.destinationPath('package.json'),
+      {
+        name: this.props.name,
+        version: '1.0.0',
+        description: this.props.description,
+        scripts: {
+          'start': 'webpack-dev-server --inline'
+        }
+      }
     );
   },
 
   install: function () {
-    this.installDependencies();
+    const devDependencies = [
+      'gulp',
+      'webpack',
+      'webpack-dev-server',
+      'ts-loader'
+    ];
+
+    const dependencies = [
+      'react',
+      'react-dom'
+    ];
+
+
   }
 });
