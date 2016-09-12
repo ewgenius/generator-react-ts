4f2de32fb863c8f5a17f769333a55b8469ea1043
ewgenius <ewgeniux@gmail.com>
2016-09-12 18:46:22 +0300

    init

---
+++ generators/app/index.js
@@ -0,0 +1,34 @@
+'use strict';
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+var yosay = require('yosay');
+
+module.exports = yeoman.Base.extend({
+  prompting: function () {
+    this.log(yosay(
+      'Welcome to the ' + chalk.red('react-ts') + ' generator!'
+    ));
+
+    var prompts = [{
+      type: 'prompt',
+      name: 'appName',
+      message: 'enter app name',
+      default: 'app'
+    }];
+
+    return this.prompt(prompts).then(function (props) {
+      this.props = props;
+    }.bind(this));
+  },
+
+  writing: function () {
+    this.fs.copy(
+      this.templatePath('dummyfile.txt'),
+      this.destinationPath('dummyfile.txt')
+    );
+  },
+
+  install: function () {
+    this.installDependencies();
+  }
+});
